194b0c9da7a0fa54b2d1eb870fce811fc6882df8
Victor Powell <vp@uber.com>
2016-07-05 01:42:53 -0700

    Add example data.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# A simple example of serving MapboxGL vector tiles in Go
+# An example Go app for dynamically serving MapboxGL vector tiles
+
+![](https://cloud.githubusercontent.com/assets/583385/16578797/4cbf4d8a-4251-11e6-9f4c-75820d220405.pn)
 
 ## Installation
 
@@ -12,14 +14,18 @@ To install, ensure github.com/golang/protobuf/proto is installed and available o
 
 ## To view the tiles
 
-To view the tiles, you'll need to modify your MapboxGL style to add the additional vector tile layer. Here's an example:
+To view the tiles, you'll need to modify your MapboxGL style to add an additional vector tile layer. Here's an example:
 
 ```
 var map = new mapboxgl.Map({
   container: 'map',
   zoom: 12.5,
   center: [-122.45, 37.79],
-  style: 'mapbox://styles/mapbox/basic-v8',
+  style: {
+    version: 8,
+    sources: {},
+    layers: []
+  },
   hash: false
 });
 
@@ -29,15 +35,28 @@ map.on('load', function loaded() {
       tiles: ['http://localhost:8080/tiles/{z}/{x}/{y}']
   });
   map.addLayer({
+    id: 'background',
+    type: 'background',
+    paint: {
+      'background-color': 'white'
+    }
+  });
+  map.addLayer({
       "id": "custom-go-vector-tile-layer",
       "type": "circle",
       "source": "custom-go-vector-tile-source",
       "source-layer": "points",
       paint: {
-        'circle-radius': 10,
-        'circle-color': 'red',
+        'circle-radius': {
+          stops: [[8, 0.1], [11, 0.5], [15, 3], [20, 20]]
+        },
+        'circle-color': '#e74c3c',
         'circle-opacity': 1
       }
   });
 });
 ```
+
+## Data from SFGov.org
+
+https://data.sfgov.org/City-Infrastructure/Street-Tree-Map/337t-q2b4
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/csv"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/vicapow/go-vtile-example/gen/third-party/vector-tile-spec/1.0.1"
@@ -27,34 +32,52 @@ func closePath(count uint32) uint32 {
 	return cmdEnc(7, count)
 }
 
-func paramEnc(value uint32) uint32 {
+func paramEnc(value int32) int32 {
 	return (value << 1) ^ (value >> 31)
 }
 
-func buildVTile() ([]byte, error) {
+func createTileWithPoints(xyz TileID, points []LngLat) ([]byte, error) {
 	tile := &vector_tile.Tile{}
-	var featureID uint64
-	var layerVersion uint32 = 1
+	var layerVersion = vector_tile.Default_Tile_Layer_Version
 	layerName := "points"
 	featureType := vector_tile.Tile_POINT
-	var layerExtent = vector_tile.Default_Tile_Layer_Extent
-  // Put a point in the center of the tile.
-	var featureGeometry = []uint32{
-		moveTo(1),
-		paramEnc(layerExtent / 2), // x
-		paramEnc(layerExtent / 2), // y
+	var extent = vector_tile.Default_Tile_Layer_Extent
+	// Put a point in the center of the tile.
+	var geometry []uint32
+	var filtered [][2]float64
+	for _, point := range points {
+		x, y := lngLatToTileXY(point, xyz)
+		if x >= 0 && x < 1 && y >= 0 && y < 1 {
+			filtered = append(filtered, [2]float64{x, y})
+		}
+	}
+	if len(filtered) > 0 {
+		cmd := moveTo(uint32(len(filtered)))
+		geometry = append(geometry, cmd)
+		var pX int32
+		var pY int32
+		for _, point := range filtered {
+			deltaX := int32(float64(extent)*point[0]+0.5) - pX
+			deltaY := int32(float64(extent)*point[1]+0.5) - pY
+			geometry = append(geometry, uint32(paramEnc(deltaX)))
+			geometry = append(geometry, uint32(paramEnc(deltaY)))
+			pX = pX + deltaX
+			pY = pY + deltaY
+		}
+	} else {
+		// Return an empty tile if we have no points
+		return nil, nil
 	}
 	tile.Layers = []*vector_tile.Tile_Layer{
 		&vector_tile.Tile_Layer{
 			Version: &layerVersion,
 			Name:    &layerName,
-			Extent:  &layerExtent,
+			Extent:  &extent,
 			Features: []*vector_tile.Tile_Feature{
 				&vector_tile.Tile_Feature{
-					Id:       &featureID,
 					Tags:     []uint32{},
 					Type:     &featureType,
-					Geometry: featureGeometry[:],
+					Geometry: geometry,
 				},
 			},
 		},
@@ -62,49 +85,123 @@ func buildVTile() ([]byte, error) {
 	return proto.Marshal(tile)
 }
 
+func xyzToLngLat(tileX float64, tileY float64, tileZ float64) (float64, float64) {
+	totalTilesX := math.Pow(2, tileZ)
+	totalTilesY := math.Pow(2, tileZ)
+	x := float64(tileX) / float64(totalTilesX)
+	y := float64(tileY) / float64(totalTilesY)
+	// lambda can go from [-pi/2, pi/2]
+	lambda := x*math.Pi*2 - math.Pi
+	// phi can go from [-1.4844, 1.4844]
+	phi := 2*math.Atan(math.Exp((2*y-1)*math.Pi)) - (math.Pi / 2)
+	lng := lambda * 180 / math.Pi
+	lat := (math.Pi - phi) * 180 / math.Pi
+	return lng, lat
+}
+
+func lngLatToTileXY(ll LngLat, tile TileID) (float64, float64) {
+	totalTilesX := math.Pow(2, float64(tile.z))
+	totalTilesY := math.Pow(2, float64(tile.z))
+	lambda := (ll.lng + 180) / 180 * math.Pi
+	// phi: [-pi/2, pi/2]
+	phi := ll.lat / 180 * math.Pi
+	tileX := lambda / (2 * math.Pi) * totalTilesX
+	// [-1.4844, 1.4844] -> [1, 0]  * totalTilesY
+	tileY := (math.Log(math.Tan(math.Pi/4-phi/2))/math.Pi/2 + 0.5) * totalTilesY
+	return tileX - float64(tile.x), tileY - float64(tile.y)
+}
+
 // Takes a string of the form `<z>/<x>/<y>` (for example, 1/2/3) and returns
 // the individual uint32 values for x, y, and z if there was no error.
 // Otherwise, err is set to a non `nil` value and x, y, z are set to 0.
-func tileToXYZ(path string) (uint32, uint32, uint32, error) {
+func tilePathToXYZ(path string) (TileID, error) {
 	xyzReg := regexp.MustCompile("(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+)")
 	matches := xyzReg.FindStringSubmatch(path)
 	if len(matches) == 0 {
-		return 0, 0, 0, errors.New("Unable to parse path as tile")
+		return TileID{}, errors.New("Unable to parse path as tile")
 	}
 	x, err := strconv.ParseUint(matches[2], 10, 32)
 	if err != nil {
-		return 0, 0, 0, err
+		return TileID{}, err
 	}
 	y, err := strconv.ParseUint(matches[3], 10, 32)
 	if err != nil {
-		return 0, 0, 0, err
+		return TileID{}, err
 	}
 	z, err := strconv.ParseUint(matches[1], 10, 32)
 	if err != nil {
-		return 0, 0, 0, err
+		return TileID{}, err
 	}
-	return uint32(x), uint32(y), uint32(z), nil
+	return TileID{x: uint32(x), y: uint32(y), z: uint32(z)}, nil
+}
+
+// A LngLat is a struct that holds a longitude and latitude vale.
+type LngLat struct {
+	lng float64
+	lat float64
+}
+
+// TileID represents the id of the tile.
+type TileID struct {
+	x uint32
+	y uint32
+	z uint32
+}
+
+// Tree a struct holder for tree information.
+type Tree struct {
+	lng     float64
+	lat     float64
+	species string
+}
+
+func loadTrees() []Tree {
+	content, err := ioutil.ReadFile("./trees.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := csv.NewReader(strings.NewReader(string(content[:])))
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// TreeID,qLegalStatus,qSpecies,qAddress,SiteOrder,qSiteInfo,PlantType,qCaretaker,qCareAssistant,PlantDate,DBH,PlotSize,PermitNotes,XCoord,YCoord,Latitude,Longitude,Location
+	var trees []Tree
+	for _, record := range records[1:] {
+		lat, _ := strconv.ParseFloat(record[15], 64)
+		lng, _ := strconv.ParseFloat(record[16], 64)
+		species := record[2]
+		trees = append(trees, Tree{lng: lng, lat: lat, species: species})
+	}
+	return trees
 }
 
 func main() {
+	trees := loadTrees()
+	points := make([]LngLat, len(trees), len(trees))
+	for i, tree := range trees {
+		points[i] = LngLat{lng: tree.lng, lat: tree.lat}
+	}
+	// points = points[0:1000]
+	fmt.Println("number of points", len(points))
 	mux := http.NewServeMux()
 
-  // Handle requests for urls of the form `/tiles/{z}/{x}/{y}` and returns
-  // the vector tile for the even tile x, y, and z coordinates.
+	// Handle requests for urls of the form `/tiles/{z}/{x}/{y}` and returns
+	// the vector tile for the even tile x, y, and z coordinates.
 	tileBase := "/tiles/"
 	mux.HandleFunc(tileBase, func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("url: %s", r.URL.Path)
 		tilePart := r.URL.Path[len(tileBase):]
-		_, _, _, err := tileToXYZ(tilePart)
+		xyz, err := tilePathToXYZ(tilePart)
 		if err != nil {
 			http.Error(w, "Invalid tile url", 400)
 			return
 		}
-		data, err := buildVTile()
+		data, err := createTileWithPoints(xyz, points)
 		if err != nil {
 			log.Fatal("error generating tile", err)
 		}
-    // All this APi to be requests from other domains.
+		// All this APi to be requests from other domains.
 		w.Header().Set("Content-Type", "application/x-protobuf")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
